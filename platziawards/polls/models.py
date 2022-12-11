572ebecc8ca406ff32a91b3eca7d310d3abc0ca4
cristianiniguez <felizpc1@gmail.com>
2022-12-11 13:40:14 -0400

    interactive shell - el metodo __str__

--- platziawards/polls/models.py
+++ platziawards/polls/models.py
@@ -1,12 +1,24 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     text = models.CharField(max_length=256)
     pub_datetime = models.DateTimeField('datetime published')
 
+    def __str__(self):
+        return self.text
+
+    def was_published_recently(self):
+        return self.pub_datetime >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=256)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
