be056d586ae60c0eec4be2d61a54a18c763e4021
cristianiniguez <felizpc1@gmail.com>
2022-12-16 11:53:51 -0400

    views - creando vistas para la aplicacion

--- platziawards/polls/views.py
+++ platziawards/polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello world')
+    return HttpResponse('You are in the main page of Platzi Awards')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are seeing the question number {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are seeing the results of question number {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting for question number {question_id}')
