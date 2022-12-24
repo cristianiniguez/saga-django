e621c0bdf45c78cd34a5c6c71958f0981b6a4144
cristianiniguez <felizpc1@gmail.com>
2022-12-24 19:59:34 -0400

    forms - creando la vista results

--- platziawards/polls/views.py
+++ platziawards/polls/views.py
@@ -15,7 +15,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f'You are seeing the results of question number {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
